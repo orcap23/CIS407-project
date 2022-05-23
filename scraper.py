b043c5b541e724ef40e7bce79330bcdb2b4930aa
RyanHeise1 <62682418+RyanHeise1@users.noreply.github.com>
2022-05-23 13:15:28 -0700

    added display func. Still needs to be completed

--- scraper.py
+++ scraper.py
@@ -1,8 +1,6 @@
 """
 Web Scraping
 
-AUTHOR: Madeline Porcaro
-
 functions:
 
 news_rss():
@@ -21,6 +19,10 @@ Takes list item and writes items to a JSON FILE.
 import json
 import requests
 from bs4 import BeautifulSoup
+from tkinter import *
+
+master = Tk()
+master.configure(bg='light grey')
 
 # scraping function
 def news_rss():
@@ -33,7 +35,6 @@ def news_rss():
         url = 'https://feeds.simplecast.com/54nAGcIl'
         r = requests.get(url)
         soup = BeautifulSoup(r.content, features='xml')
-        
         
         items = soup.findAll('item')
 
@@ -62,8 +63,42 @@ def news_rss():
         print('Scraping Failed. See exception:')
         print(e)
 
+def display():
+    # object of tkinter
+    # and background set to grey
+    master = Tk()
+    master.configure(bg='light grey')
+    
+    # Variable Classes in tkinter
+    title = StringVar();
+    meta = StringVar();
+    art_dec = StringVar();
+    
+    # Creating label for each information
+    # name using widget Label
+    Label(master, text="Title :",
+        bg = "light grey").grid(row=3, sticky=W)
+    Label(master, text="Meta information :",
+        bg = "light grey").grid(row=4, sticky=W)
+    Label(master, text="Article description :",
+        bg = "light grey").grid(row=5, sticky=W)
+    
+    # Creating label for class variable
+    # name using widget Entry
+    Label(master, text="", textvariable=title,
+        bg = "light grey").grid(row=3,column=1, sticky=W)
+    Label(master, text="", textvariable=meta,
+        bg = "light grey").grid(row=4,column=1, sticky=W)
+    Label(master, text="", textvariable=art_dec,
+        bg = "light grey").grid(row=5,column=1, sticky=W)
+    mainloop()
+
+
 # use json to write to text file
 def save_info(article_list):
+    for elem in article_list:
+        print(f"{elem}\n\n")
+        
     json_object = json.dumps(article_list, indent=4, sort_keys=True)
     with open('rss.json', 'w') as outfile:
         outfile.write(json_object)
